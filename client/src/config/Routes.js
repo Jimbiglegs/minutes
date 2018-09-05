9e94677ddd1ffa15d5975bc6b69cd917c23fbc3d
Niti Singhal <gupta.sweet.niti@gmail.com>
2018-09-04 21:54:34 -0700

    restricted routes

--- client/src/config/Routes.js
+++ client/src/config/Routes.js
@@ -1,7 +1,7 @@
 import React, { Component }from 'react';
 import {Switch, Route} from 'react-router-dom';
 import  Landing from '../views/Landing';
-import homepage from '../views/HomePage';
+import HomePage from '../views/HomePage';
 import Meetings from '../views/Meetings';
 import TaskView from '../views/TaskView';
 import CreateNotes from '../views/CreateNotes';
@@ -15,17 +15,55 @@ class Routes extends Component{
     render() {
         return <Switch>
             <Route exact path="/" component={ Landing } />
-            <Route exact path="/home" component={ homepage } />
-            <Route exact path="/meetings" component={ Meetings } />
-            <Route exact path="/tasks" component={ TaskView } />
-            <Route exact path="/teams" component={ TeamsView } />
+            <Route exact path="/home" render={ () => {
+                    if(!this.props.user) {
+                        this.props.history.push('/');
+                        return null;
+                    }                   
+                    return <HomePage />
+                } } />
+            <Route exact path="/meetings" render={ () => {
+                    if(!this.props.user) {
+                        this.props.history.push('/');
+                        return null;
+                    }                   
+                    return <Meetings />
+                } } />
+            <Route exact path="/tasks" render={ () => {
+                    if(!this.props.user) {
+                        this.props.history.push('/');
+                        return null;
+                    }                   
+                    return <TaskView />
+                } } />
+            <Route exact path="/teams" render={ () => {
+                    if(!this.props.user) {
+                        this.props.history.push('/');
+                        return null;
+                    }                   
+                    return <TeamsView />
+                } } />
             <Route exact path="/adhocMeeting" render={ () => {
+                if(!this.props.user) {
+                    this.props.history.push('/');
+                    return null;
+                } 
                 return <CreateNotes key='adhoc' />
             } } />
             <Route exact path="/createNotes" render={ () => {
+                if(!this.props.user) {
+                    this.props.history.push('/');
+                    return null;
+                }                 
                 return <CreateNotes key='create' />
             } } />
-            <Route exact path="/scheduleMeeting" component={ Schedule } />
+            <Route exact path="/scheduleMeeting" render={ () => {
+                if(!this.props.user) {
+                    this.props.history.push('/');
+                    return null;
+                }                 
+                return <Schedule />
+            } } />
         </Switch>;
     }
 }
