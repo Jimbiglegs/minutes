fcf8c6fe7a8c4833644127e42eb263b69dbfb953
Niti Singhal <gupta.sweet.niti@gmail.com>
2018-09-05 20:17:29 -0700

    integrate slack serverside

--- server/utils.js
+++ server/utils.js
@@ -1,3 +1,7 @@
+const slackUrl = 'https://hooks.slack.com/services/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';
+const { IncomingWebhook } = require('@slack/client');
+const webhook = new IncomingWebhook(slackUrl);
+
 // file contains code repeated amongst multiple files
 function isEmpty(obj) {
     if(!obj || obj === null) {
@@ -13,6 +17,17 @@ function isEmpty(obj) {
     }
 
     return false;
+}
+
+function sendSlack(message) {
+    webhook.send(message, function(err, res) {
+        if (err) {
+            console.log('Error sending slack message:', err);
+            return;
+        }
+        
+        console.log('slack Message sent: ', res);
+    });
 }
 
 //exporting it make it available to other files
