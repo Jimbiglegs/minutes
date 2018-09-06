b4e3f314db7c930f6bc136ea5eeb4f539ac20504
Niti Singhal <gupta.sweet.niti@gmail.com>
2018-09-06 16:57:17 -0700

    removed attribute link from footer

--- client/src/component/Footer.js
+++ client/src/component/Footer.js
@@ -3,8 +3,7 @@ import React, {Component} from 'react';
 export default class Footer extends Component{
     render(){
         return <footer role="contentinfo" className="mt-auto bg-light">
-            <a href="https://clipartxtras.com/">clipartxtras.com</a>
-            <a>Copyright (c) 2018, Niti Singhal. </a>
+            Copyright (c) 2018, Niti Singhal.
         </footer>;
     }
 }
